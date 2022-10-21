b004386b18f72964ab55b8f50e99037a10e7512d
Petri Hoskonen <petri.hoskonen@mediconsult.fi>
2022-10-21 14:31:10 +0300

    File watching, renaming, copying and deleting.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,33 @@
 import watch from 'node-watch';
+import * as fs from 'fs';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+
+enum operationMode {
+    UPDATE = 'update',
+    REMOVE = 'remove'
+}
+
+const newFileName = 'texgen-renamed.zip';
 
 // set your source folder
-const source1 = '';
-// const source2 = '';
+const source1 = 'C:\\Users\\petri.hoskonen\\Desktop\\source\\';
+const destination1 = 'C:\\Users\\petri.hoskonen\\Desktop\\destination\\';
 
-watch(source1, { recursive: true, filter: /\.zip$/ }, function(evt, name) {
-    console.log('%s changed.', name);
+console.log('file watchers starting..');
+watch(source1, { recursive: true, filter: /\.zip$/ }, function(event, fileName) {
+    if (fileName && event === operationMode.UPDATE) {
+        console.log('%s appeared for processing..', fileName);
+
+        // copy file to our destination
+        fs.copyFile(fileName, destination1 + newFileName, function(error) {
+            if (error) {
+                throw error;
+            }
+
+            // remove the copied file
+            fs.unlinkSync(fileName);
+
+            console.log('file renamed and moved succefully!');
+        })
+    }
   });
